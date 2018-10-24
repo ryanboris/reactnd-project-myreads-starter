076d4b96ac586b66aa7fe057354dbce4d5aff36e
ryanboris <ryan.boris@gmail.com>
2018-10-23 23:12:03 -0400

    Feat: Creates BookShelf component to make and populate each shelf

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import Book from "./Book";
+import { PropTypes } from "prop-types";
 
-const Bookshelf = props => {
-  return (
-    <div className="book-shelf-changer">
-      <select>
-        <option value="move" disabled>
-          Move to...
-        </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-      </select>
-    </div>
-  );
+export default class BookShelf extends Component {
+  update_book = (book, shelf) => {
+    this.props.onChangeShelf(book, shelf);
+  };
+  render() {
+    const books = this.props.books;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book, index) => (
+              <Book
+                book={book}
+                key={index}
+                onUpdate={shelf => {
+                  this.update_book(book, shelf);
+                }}
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
 };
-
-export default Bookshelf;
