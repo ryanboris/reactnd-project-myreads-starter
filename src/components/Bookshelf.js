8bc334e3185460c2fc0cbf48d7aa50692273fce8
ryanboris <ryan.boris@gmail.com>
2018-11-08 17:18:11 -0500

    Feat: Refactor BookShelf into a stateless component

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
-import Book from "./Book";
-import { PropTypes } from "prop-types";
+import React, { Component } from 'react';
+import Book from './Book';
+import { PropTypes } from 'prop-types';
 
-export default class BookShelf extends Component {
+const BookShelf = props => {
   update_book = (book, shelf) => {
-    this.props.onChangeShelf(book, shelf);
+    props.onChangeShelf(book, shelf);
   };
-  render() {
-    const books = this.props.books;
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book, index) => (
-              <Book
-                book={book}
-                key={index}
-                onUpdate={shelf => {
-                  this.update_book(book, shelf);
-                }}
-              />
-            ))}
-          </ol>
-        </div>
+  const books = this.props.books;
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book, index) => (
+            <Book
+              book={book}
+              key={index}
+              onUpdate={shelf => {
+                this.update_book(book, shelf);
+              }}
+            />
+          ))}
+        </ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 BookShelf.propTypes = {
   books: PropTypes.array.isRequired,
